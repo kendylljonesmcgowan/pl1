352a5b36bc7a2b0f942ccbb0157d6f2645f6643e
kendylljonesmcgowan <91283798+kendylljonesmcgowan@users.noreply.github.com>
2022-02-01 19:18:14 -0500

    Update javascript.js

--- story/javascript.js
+++ story/javascript.js
@@ -86,4 +86,53 @@ function initMap() {
       },
     ],
   });
+  
+    setMarkers(map);
+}
+
+// Data for the markers consisting of a name, a LatLng and a zIndex for the
+// order in which these markers should display on top of each other.
+const beaches = [
+  ["Mystic Beach Trailhead", 48.438, -124.0927, 1],
+  ["Panorama Ridge Trailhead", 49.954, -123.0135, 2],
+  ["Matthew's Head Trailhead", 45.566, -64.984, 3],
+  ["St. Peter's Harbour Lighthouse", 46.443, -62.746, 4],
+  ["Ol' Reliable", 44.357, -78.724, 5],
+];
+
+function setMarkers(map) {
+  // Adds markers to the map.
+  // Marker sizes are expressed as a Size of X,Y where the origin of the image
+  // (0,0) is located in the top left of the image.
+  // Origins, anchor positions and coordinates of the marker increase in the X
+  // direction to the right and in the Y direction down.
+  const image = {
+    url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+    // This marker is 20 pixels wide by 32 pixels high.
+    size: new google.maps.Size(20, 32),
+    // The origin for this image is (0, 0).
+    origin: new google.maps.Point(0, 0),
+    // The anchor for this image is the base of the flagpole at (0, 32).
+    anchor: new google.maps.Point(0, 32),
+  };
+  // Shapes define the clickable region of the icon. The type defines an HTML
+  // <area> element 'poly' which traces out a polygon as a series of X,Y points.
+  // The final coordinate closes the poly by connecting to the first coordinate.
+  const shape = {
+    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    type: "poly",
+  };
+
+  for (let i = 0; i < beaches.length; i++) {
+    const beach = beaches[i];
+
+    new google.maps.Marker({
+      position: { lat: beach[1], lng: beach[2] },
+      map,
+      icon: image,
+      shape: shape,
+      title: beach[0],
+      zIndex: beach[3],
+    });
+  }
 }
