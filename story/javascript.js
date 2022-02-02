da48e9a95af285de94b06bc6229c2a4ebaa6a095
kendylljonesmcgowan <91283798+kendylljonesmcgowan@users.noreply.github.com>
2022-02-02 18:53:49 -0500

    Update javascript.js

--- story/javascript.js
+++ story/javascript.js
@@ -3,7 +3,7 @@
 function initMap() {
   // Styles a map in night mode.
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 57.348, lng: -102.497 },
+    center: { lat: 56.313, lng: -97.572 },
     zoom: 4,
     styles: [
       { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
