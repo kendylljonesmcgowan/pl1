50fedd9e56bf9f0f3ecb458b40efa2e31955ac6c
kendylljonesmcgowan <91283798+kendylljonesmcgowan@users.noreply.github.com>
2022-02-02 20:59:37 -0500

    Update javascript.js

--- story/javascript.js
+++ story/javascript.js
@@ -4,11 +4,10 @@
 // please enjoy a map of some nice walks
 
 // JavaScript for Night Mode custom Google Map inspired by: https://developers.google.com/maps/documentation/javascript/examples/style-array#maps_style_array-javascript
-// JavaScript for clustered markers inspired by: https://developers.google.com/maps/documentation/javascript/marker-clustering#maps_marker_clustering-javascript
-
+// JavaScript for infowindow inspired by: https://developers.google.com/maps/documentation/javascript/infowindows#open
 
 function initMap() {
-// Styles the map in night-mode
+// styles the map in night-mode
   const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 60.970, lng: -89.578 },
     zoom: 4,
@@ -93,18 +92,19 @@ function initMap() {
       },
     ],
   });
-  
+ 
+// sets content of "Ol' Reliable walk infowindow, generates infowindow, generates and tethers infowindow to marker
  const contentString =
     '<div id="content">' +
     '<div id="siteNotice">' +
     "</div>" +
     '<h1 id="firstHeading" class="firstHeading">Ol Reliable</h1>' +
     '<div id="bodyContent">' +
-    "<p>This is the trail I walk almost every day. Walking is very important to me, especially since COVID started. " +
-    "I thought I would make a map dedicated to " +
-    "my top 5 favourite walks in Canada. " +
-    "I hope you enjoy, and I hope you get to go on some of these walks " +
-    "one day too!</p>" +
+    "<p>This is the trail I walk almost every day. Walking is a very important " +
+    "part of my everyday life, especially since COVID started. So, I thought I would " +
+    "make a map dedicated to my top 4 favourite walks in Canada (so far...). " +
+    "I hope you enjoy, and I hope you get to go on some of these walks one day too! " +
+    "Trail names from left to right: Mystic Beach Trailhead, Panorama Ridge Trailhead, Matthew's Head Trailhead, and St. Peter's Harbour Lighthouse</p>" +
     "</div>" +
     "</div>";
   const infowindow = new google.maps.InfoWindow({
@@ -123,11 +123,33 @@ function initMap() {
       shouldFocus: false,
     });
   }); 
-  
+ 
+// makes Mystic Beach marker
   new google.maps.Marker({
     position: { lat: 48.438, lng: -124.0927},
     map,
     title: "Mystic Beach Trailhead",
+  });
+
+// makes Panorama Ridge marker
+    new google.maps.Marker({
+    position: { lat: 49.954, lng: -123.0135},
+    map,
+    title: "Panorama Ridge Trailhead",
+  });
+
+// makes Matthew's Trailhead marker
+    new google.maps.Marker({
+    position: { lat: 45.566, lng: -64.984},
+    map,
+    title: "Matthew's Head Trailhead",
+  });
+  
+  // makes St. Peter's Harbour marker
+    new google.maps.Marker({
+    position: { lat: 46.443, lng: -62.746},
+    map,
+    title: "St. Peter's Harbour Lighthouse",
   });
 
 }
