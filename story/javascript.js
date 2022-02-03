c16399e82aefe0511ca2141037c87ec23bf88bd0
kendylljonesmcgowan <91283798+kendylljonesmcgowan@users.noreply.github.com>
2022-02-02 20:08:56 -0500

    Update javascript.js

--- story/javascript.js
+++ story/javascript.js
@@ -6,6 +6,8 @@
 // JavaScript for Night Mode custom Google Map inspired by: https://developers.google.com/maps/documentation/javascript/examples/style-array#maps_style_array-javascript
 // JavaScript for clustered markers inspired by: https://developers.google.com/maps/documentation/javascript/marker-clustering#maps_marker_clustering-javascript
 
+let marker;
+
 function initMap() {
 // Styles the map in night-mode
   const map = new google.maps.Map(document.getElementById("map"), {
@@ -92,4 +94,23 @@ function initMap() {
       },
     ],
   });
+  
+    marker = new google.maps.Marker({
+    map,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    position: { lat: 59.327, lng: 18.067 },
+  });
+  marker.addListener("click", toggleBounce);
 }
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+}
+
+
