d700862643c1c9c199f3c4a08599a7d2c69b50d7
kendylljonesmcgowan <91283798+kendylljonesmcgowan@users.noreply.github.com>
2022-02-02 20:20:39 -0500

    Update javascript.js

--- story/javascript.js
+++ story/javascript.js
@@ -6,7 +6,6 @@
 // JavaScript for Night Mode custom Google Map inspired by: https://developers.google.com/maps/documentation/javascript/examples/style-array#maps_style_array-javascript
 // JavaScript for clustered markers inspired by: https://developers.google.com/maps/documentation/javascript/marker-clustering#maps_marker_clustering-javascript
 
-let marker;
 
 function initMap() {
 // Styles the map in night-mode
@@ -95,21 +94,44 @@ function initMap() {
     ],
   });
   
-    marker = new google.maps.Marker({
-    map,
-    draggable: true,
-    animation: google.maps.Animation.DROP,
-    position: { lat: 48.438, lng: -124.0927 },
+ const contentString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+    "sandstone rock formation in the southern part of the " +
+    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+    "south west of the nearest large town, Alice Springs; 450&#160;km " +
+    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+    "Aboriginal people of the area. It has many springs, waterholes, " +
+    "rock caves and ancient paintings. Uluru is listed as a World " +
+    "Heritage Site.</p>" +
+    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+    "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+    "(last visited June 22, 2009).</p>" +
+    "</div>" +
+    "</div>";
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
   });
-  marker.addListener("click", toggleBounce);
-}
+  const marker = new google.maps.Marker({
+    position: { lat: 48.438, lng: -124.0927 },
+    map,
+    title: "Mystic Beach Trailhead",
+  });
 
-function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-  }
+  marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+      shouldFocus: false,
+    });
+  }); 
+
 }
 
 
