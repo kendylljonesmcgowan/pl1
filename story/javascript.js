61b01f93d8ec47745e98b36571147cf6197983c6
kendylljonesmcgowan <91283798+kendylljonesmcgowan@users.noreply.github.com>
2022-02-02 19:49:32 -0500

    Update javascript.js

--- story/javascript.js
+++ story/javascript.js
@@ -93,21 +93,3 @@ function initMap() {
         stylers: [{ color: "#17263c" }],
       },
     ],
-    
-    let marker;
-    marker = new google.maps.Marker({
-    map,
-    draggable: true,
-    animation: google.maps.Animation.DROP,
-    position: { lat: 59.327, lng: 18.067 },
-  });
-  marker.addListener("click", toggleBounce);
-}
-
-function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-  }
-}
