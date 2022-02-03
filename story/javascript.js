d4dc76e0afae79f7de51fbf227c30685a61676a6
kendylljonesmcgowan <91283798+kendylljonesmcgowan@users.noreply.github.com>
2022-02-02 19:42:19 -0500

    Update javascript.js

--- story/javascript.js
+++ story/javascript.js
@@ -6,6 +6,8 @@
 // JavaScript for Night Mode custom Google Map inspired by: https://developers.google.com/maps/documentation/javascript/examples/style-array#maps_style_array-javascript
 // JavaScript for clustered markers inspired by: https://developers.google.com/maps/documentation/javascript/marker-clustering#maps_marker_clustering-javascript
 
+let marker;
+
 function initMap() {
 // Styles the map in night-mode
   const map = new google.maps.Map(document.getElementById("map"), {
@@ -92,29 +94,19 @@ function initMap() {
       },
     ],
     
- // Create an array of alphabetical characters used to label the markers.
-  const labels = "ABCDE";
-  // Add some markers to the map.
-  // Note: The code uses the JavaScript Array.prototype.map() method to
-  // create an array of markers based on a given "locations" array.
-  // The map() method here has nothing to do with the Google Maps API.
-  const markers = locations.map((location, i) => {
-    return new google.maps.Marker({
-      position: location,
-      label: labels[i % labels.length],
-    });
+   marker = new google.maps.Marker({
+    map,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    position: { lat: 48.438, lng: -124.0927 },
   });
-  // Add a marker clusterer to manage the markers.
-  new MarkerClusterer(map, markers, {
-    imagePath:
-      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-  });
+  marker.addListener("click", toggleBounce);
 }
-const locations = [
-{ lat: 48.438, lng: -124.0927},
-{ lat: 49.954, lng: -123.0135},
-{ lat: 45.566, lng: -64.984},
-{ lat: 46.443, lng: -62.746},
-{ lat: 44.357, lng: -78.724},
-  
-];
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
